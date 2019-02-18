76429906bf2f477d6eea03f4bd80cdded4ce2455
Johannes Rappen <jrappen@outlook.com>
2019-02-18 08:47:29 +0100

    chore: Enhance readability of src/wkhtmltopdf.py

--- src/wkhtmltopdf.py
+++ src/wkhtmltopdf.py
@@ -30,7 +30,10 @@ def load_settings(reload=False):
         PKG_PREF = NamedSettingsDict(PKG_NAME)
         PKG_PREF.subscribe(
             load_settings(reload=True),
-            default_value=ResourcePath('Packages/{}/.sublime/settings/{}.sublime-settings'.format(PKG_NAME, PKG_NAME)).read_bytes()
+            default_value=ResourcePath(
+                'Packages/{}/.sublime/settings/{}.sublime-settings'
+                .format(PKG_NAME, PKG_NAME)
+            ).read_bytes()
         )
     except Exception as e:
         print(e)
